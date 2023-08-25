07aad7373013a0d85cbd1d8561f16b521422b21c
LaidBenglia <73167661+LaidBengli@users.noreply.github.com>
2023-08-25 07:01:44 -0700

    Done

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  console.log(word.split("").reverse().join(""))
+  if (word.split("").reverse().join("") === word) return true
+  else return false;
 }
-
+isPalindrome('Laid')
 /* 
   Add your pseudocode here
 */
@@ -20,6 +23,15 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("racecar"));
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("mom"));
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("ahmed"));
 }
 
 module.exports = isPalindrome;
